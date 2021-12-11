d21ed3da9cf215378b9d04dfefc33de8756e7820
Shaohao <shaohaolinca@gmail.com>
2021-12-10 22:26:22 -0800

    day 11

---
+++ 11/part2.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const filename = process.argv[2];
+const file = fs.readFileSync(filename).toString("utf8");
+console.log("filename:", filename);
+
+const inputs = file
+  .split("\n")
+  .map((line) => line.split("").map((x) => parseInt(x)));
+
+const gridSize = 10;
+let step = 0;
+let hasFlashPoint = [];
+let numberOfFlashed = 0;
+
+while (inputs.flat().some((x) => x !== 0)) {
+  hasFlashPoint = [];
+  step++;
+  for (let rowIndex = 0; rowIndex < inputs.length; rowIndex++) {
+    const row = inputs[rowIndex];
+    for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+      increseValue(rowIndex, columnIndex);
+    }
+  }
+  console.log(`After step ${step}`);
+  console.log(inputs);
+}
+
+console.log(`# of flashed: ${numberOfFlashed}`);
+
+function increseValue(rowIndex, columnIndex) {
+  const element = inputs[rowIndex][columnIndex];
+  if (element === 9) {
+    inputs[rowIndex][columnIndex] = 0;
+    hasFlashPoint.push({
+      rowIndex,
+      columnIndex,
+    });
+    numberOfFlashed++;
+    increaseNeighbour(rowIndex, columnIndex);
+  } else {
+    if (
+      hasFlashPoint.some(
+        (point) =>
+          point.rowIndex === rowIndex && point.columnIndex === columnIndex
+      )
+    ) {
+      // console.log(`octopus at x = ${rowIndex} y = ${columnIndex} flashed!`);
+    } else {
+      inputs[rowIndex][columnIndex] += 1;
+    }
+  }
+}
+
+function increaseNeighbour(rowIndex, columnIndex) {
+  if (rowIndex - 1 >= 0) {
+    // (x-1, y)
+    increseValue(rowIndex - 1, columnIndex);
+
+    if (columnIndex - 1 >= 0) {
+      // (x-1, y-1)
+      increseValue(rowIndex - 1, columnIndex - 1);
+    }
+
+    if (columnIndex + 1 < gridSize) {
+      // (x-1, y+1)
+      increseValue(rowIndex - 1, columnIndex + 1);
+    }
+  }
+
+  if (rowIndex + 1 < gridSize) {
+    // (x + 1, y)
+    increseValue(rowIndex + 1, columnIndex);
+
+    if (columnIndex - 1 >= 0) {
+      // (x+1, y-1)
+      increseValue(rowIndex + 1, columnIndex - 1);
+    }
+
+    if (columnIndex + 1 < gridSize) {
+      // (x+1, y+1)
+      increseValue(rowIndex + 1, columnIndex + 1);
+    }
+  }
+
+  if (columnIndex - 1 >= 0) {
+    // (x, y-1)
+    increseValue(rowIndex, columnIndex - 1);
+  }
+
+  if (columnIndex + 1 < gridSize) {
+    // (x, y + 1)
+    increseValue(rowIndex, columnIndex + 1);
+  }
+}
